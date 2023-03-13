61d05c64207f44c93162f73b28fa1c63e3720ec4
PepperJackExA <pepperjack.exa@gmail.com>
2023-03-13 15:48:13 +0300

    upload the adjuster files

---
+++ PEP CO Low power Steam for IO/Data/Scripts/Adjuster/BlockDefinitions.cs
@@ -0,0 +1,327 @@
+﻿using System.Collections.Generic;
+using static ModAdjuster.DefinitionStructure;
+using static ModAdjuster.DefinitionStructure.BlockDef;
+using static ModAdjuster.DefinitionStructure.BlockDef.BlockAction.BlockMod;
+
+namespace ModAdjuster
+{
+    public class BlockDefinitions
+    {
+
+        public List<BlockDef> Definitions = new List<BlockDef>()
+        {
+            // Disabled blocks
+            new BlockDef()
+            {
+                BlockName = "OxygenGenerator/CoalFurnace",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "You should not see this block"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeBlockPublicity // Sets <Public> to the opposite of its setting in sbc
+                    },
+                    new BlockAction
+                    {
+                        Action = ReplaceComponent, // Replaces the component at the given index with a new component
+                        Component = "Component/ZoneChip",
+                        Index = 0,
+                        Count = 1 // This field is optional. If not specified or set to 0, the component count will stay the same
+                    }
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "WindTurbine/LargeWindTurbine",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "You should not see this block"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeBlockPublicity // Sets <Public> to the opposite of its setting in sbc
+                    },
+                    new BlockAction
+                    {
+                        Action = ReplaceComponent, // Replaces the component at the given index with a new component
+                        Component = "Component/ZoneChip",
+                        Index = 0,
+                        Count = 1 // This field is optional. If not specified or set to 0, the component count will stay the same
+                    }
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "OxygenFarm/SolarConcentrator",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "You should not see this block"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeBlockPublicity // Sets <Public> to the opposite of its setting in sbc
+                    },
+                    new BlockAction
+                    {
+                        Action = ReplaceComponent, // Replaces the component at the given index with a new component
+                        Component = "Component/ZoneChip",
+                        Index = 0,
+                        Count = 1 // This field is optional. If not specified or set to 0, the component count will stay the same
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "HydrogenEngine/SteamTurbineMirrored",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "Pressurized Steam Turbine for generating electricity. Must be connected to a source of Steam. Consumes 100 Steam/sec. Max Output: 100MW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 100f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "HydrogenEngine/SteamTurbine",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "Pressurized Steam Turbine for generating electricity. Must be connected to a source of Steam. Consumes 100 Steam/sec. Max Output: 100MW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 100f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "SolarPanel/LargeFullSpectrumSolarPanel",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "High-tech panel capable of generating power from all electromagnetic wavelengths at high efficiency. Max Output: 1MW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 1.5f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "SolarPanel/SmallFullSpectrumSolarPanel",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "High-tech panel capable of generating power from all electromagnetic wavelengths at high efficiency. Max Output: 300kW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 0.3f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "SolarPanel/LargeBlockSolarPanel",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "Basic Solar panel capable of generating power. Max Output: 500kW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 0.5f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "SolarPanel/SmallBlockSolarPanel",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "Basic Solar panel capable of generating power. Max Output: 150kW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 0.15f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "WindTurbine/LargeBlockWindTurbine",
+                BlockActions = new[]
+                {
+                    
+                    new BlockAction
+                    {
+                        Action = ChangeBlockName, // Change Display Name of the block
+                        NewText = "Basic Wind Turbine"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "Basic Solar panel capable of generating power. Nominal Max Output: 500kW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 0.5f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "WindTurbine/Serg_5MW_Vertical_Wind_Turbine",
+                BlockActions = new[]
+                {
+                    
+                    new BlockAction
+                    {
+                        Action = ChangeBlockName, // Change Display Name of the block
+                        NewText = "Vertical Wind Turbine"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "Basic Solar panel capable of generating power. Nominal Max Output: 1.5MW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 0.15f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "WindTurbine/Serg_5MW_Vertical_Wind_Turbine_Top",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockName, // Change Display Name of the block
+                        NewText = "Vertical Wind Turbine Top"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "Basic Solar panel capable of generating power. Nominal Max Output: 1.5MW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 1.5f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "WindTurbine/Serg_10MW_Wind_Turbine",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockName, // Change Display Name of the block
+                        NewText = "Large Vertical Wind Turbine"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "Basic Solar panel capable of generating power. Nominal Max Output: 3MW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 3f
+                    },
+
+                }
+
+            },
+            new BlockDef()
+            {
+                BlockName = "WindTurbine/Serg_25MW_Wind_Turbine",
+                BlockActions = new[]
+                {
+                    new BlockAction
+                    {
+                        Action = ChangeBlockName, // Change Display Name of the block
+                        NewText = "XL Wind Turbine"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeBlockDescription, // Change the Description of the block
+                        NewText = "Extra Large Wind Turbine capable of generating power. Nominal Max Output: 10MW"
+                    },
+                    new BlockAction
+                    {
+                        Action = ChangeMaxPowerOutput, // Sets <MaxPowerOutput> for any reactor, hydrogen engine, solar panel, wind turbine, or battery
+                        Value = 10f
+                    },
+
+                }
+
+            },
+
+        };
+
+    }
+}
---
+++ PEP CO Low power Steam for IO/Data/Scripts/Adjuster/BlueprintDefinitions.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using static ModAdjuster.DefinitionStructure;
+using static ModAdjuster.DefinitionStructure.BlueprintDef;
+using static ModAdjuster.DefinitionStructure.BlueprintDef.BPAction.BPMod;
+
+namespace ModAdjuster
+{
+    public class BlueprintDefinitions
+    {
+        public List<BlueprintDef> Definitions = new List<BlueprintDef>()
+        {   
+            // List of blueprints to modify. Can be as many or few as desired
+            new BlueprintDef()
+            {
+                BlueprintName = "MyObjectBuilder_BlueprintDefinition/40mmATLASBP", // Name of the blueprint to modify
+                BPActions = new[] // List of modifications to make. Can be as many or few as desired
+                {
+                    new BPAction
+                    {
+                        Action = ChangeAmountPrerequisite, // Change the required amount of the item at the given index
+                        Index = 0,
+                        Amount = 50f
+                    },
+                    new BPAction
+                    {
+                        Action = ReplacePrerequisite, // Replace the required item at the given index with a new item
+                        Index = 2,
+                        Item = "MyObjectBuilder_Ingot/Cobalt"
+                    },
+                    new BPAction
+                    {
+                        Action = InsertPrerequisite, //Insert the given amount of a new required item at the given index
+                        Index = 3,
+                        Item = "MyObjectBuilder_Ingot/Platinum",
+                        Amount = 5f
+                    },
+                }
+            },
+
+            new BlueprintDef()
+            {
+                BlueprintName = "MyObjectBuilder_BlueprintDefinition/StoneOreToIngot",
+                BPActions = new[]
+                {
+                    new BPAction
+                    {
+                        Action = ChangeAmountResult, // Change the produced amount of the item at the given index
+                        Index = 0,
+                        Amount = 20f
+                    },
+                    new BPAction
+                    {
+                        Action = ReplaceResult, // Replace the produced item at the given index with a new item
+                        Index = 3,
+                        Item = "MyObjectBuilder_Ingot/Cobalt"
+                    },
+                    new BPAction
+                    {
+                        Action = InsertResult, //Insert the given amount of a new produced item at the given index
+                        Index = 4,
+                        Item = "MyObjectBuilder_Ingot/Silver",
+                        Amount = 1.5f
+                    },
+                }
+            },
+
+            new BlueprintDef()
+            {
+                BlueprintName = "MyObjectBuilder_BlueprintDefinition/Concrete", // Name of the blueprint to modify
+                BPActions = new[] // List of modifications to make. Can be as many or few as desired
+                {
+                    new BPAction
+                    {
+                        Action = ChangeBpPublicity, // Sets <Public> to the opposite of its setting in sbc
+                    },
+                    new BPAction
+                    {
+                        Action = ChangeBpDisplayName, // Change the Display Name of the blueprint
+                        Item = "Hardened Concrete",
+                    },
+                    new BPAction
+                    {
+                        Action = ChangeProductionTime,
+                        Amount = 22.5f,
+                    },
+                }
+            },
+        };
+    }
+}
