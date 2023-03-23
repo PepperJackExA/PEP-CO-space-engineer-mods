830efb6e36f14bddca32941f990a34a920c2068d
PepperJackExA <pepperjack.exa@gmail.com>
2023-03-23 18:03:30 +0300

    Work in progress: Added Mod Adjuster.

---
+++ 2939485512 - Ores First/Data/Scripts/Adjuster/BlueprintDefinitions.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using static ModAdjuster.DefinitionStructure;
+using static ModAdjuster.DefinitionStructure.BlueprintDef;
+using static ModAdjuster.DefinitionStructure.BlueprintDef.BPAction.BPMod;
+
+namespace ModAdjuster
+{
+    public class BlueprintDefinitions
+    {
+        public List<BlueprintDef> Definitions = new List<BlueprintDef>()
+        {   
+            
+
+            new BlueprintDef()
+            {
+                BlueprintName = "MyObjectBuilder_BlueprintDefinition/StoneOreToIngotStarting",
+                BPActions = new[]
+                {
+                    new BPAction
+                    {
+                        Action = ReplaceResult, 
+                        Index = 0,
+                        Item = "MyObjectBuilder_Ingot/Cobalt"
+                    },
+                    new BPAction
+                    {
+                        Action = ReplaceResult, 
+                        Index = 1,
+                        Item = "MyObjectBuilder_Ingot/Silver",
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult, 
+                        Index = 2,
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult,
+                        Index = 3,
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult, 
+                        Index = 4,
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult, 
+                        Index = 5,
+                    },
+                }
+            },
+            new BlueprintDef()
+            {
+                BlueprintName = "MyObjectBuilder_BlueprintDefinition/StoneOreToIngotBasic",
+                BPActions = new[]
+                {
+                    new BPAction
+                    {
+                        Action = ReplaceResult,
+                        Index = 0,
+                        Item = "MyObjectBuilder_Ingot/Cobalt"
+                    },
+                    new BPAction
+                    {
+                        Action = ReplaceResult,
+                        Index = 1,
+                        Item = "MyObjectBuilder_Ingot/Silver",
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult,
+                        Index = 2,
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult,
+                        Index = 3,
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult,
+                        Index = 4,
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult,
+                        Index = 5,
+                    },
+                }
+            },
+            new BlueprintDef()
+            {
+                BlueprintName = "MyObjectBuilder_BlueprintDefinition/StoneOreToIngot",
+                BPActions = new[]
+                {
+                    new BPAction
+                    {
+                        Action = ReplaceResult,
+                        Index = 0,
+                        Item = "MyObjectBuilder_Ingot/Cobalt"
+                    },
+                    new BPAction
+                    {
+                        Action = ReplaceResult,
+                        Index = 1,
+                        Item = "MyObjectBuilder_Ingot/Silver",
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult,
+                        Index = 2,
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult,
+                        Index = 3,
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult,
+                        Index = 4,
+                    },
+                    new BPAction
+                    {
+                        Action = RemoveResult,
+                        Index = 5,
+                    },
+                }
+            },
+
+        };
+    }
+}
