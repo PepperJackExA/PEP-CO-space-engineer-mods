01f94d8b4156b4d90ac62cf29731e6a563045ecc
PepperJackExA <pepperjack.exa@gmail.com>
2023-04-01 18:35:12 +0300

    initial release

---
+++ 2955938343 - Limited Voxel Placement - PEP CO/Data/Scripts/Limited_Voxel_Placement/PEPCO_Limited_Voxel_Placement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Sandbox.Definitions;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ObjectBuilders.Definitions.SessionComponents;
+
+namespace PEPCO_Limited_Voxel_Placement
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class Example_EditCharacterDef : MySessionComponentBase
+    {
+
+        public override void LoadData()
+        {
+            foreach (var def in MyDefinitionManager.Static.GetAllDefinitions())
+            {
+                var blockDef = def as MyCubeBlockDefinition;
+                if (blockDef != null)
+                {
+                    // ignore all CubeBlock and BatteryBlock types
+                    if (blockDef.Id.TypeId == typeof(MyObjectBuilder_CubeBlock))
+                        continue;
+                    if (blockDef.Id.TypeId == typeof(MyObjectBuilder_BatteryBlock))
+                        continue;
+
+                    blockDef.VoxelPlacement = new VoxelPlacementOverride()
+                    {
+                        DynamicMode = new VoxelPlacementSettings()
+                        {
+                            PlacementMode = VoxelPlacementMode.OutsideVoxel,
+                        },
+                        StaticMode = new VoxelPlacementSettings()
+                        {
+                            PlacementMode = VoxelPlacementMode.OutsideVoxel,
+                        },
+                    };
+                }
+            }
+        }   
+    }
+}
