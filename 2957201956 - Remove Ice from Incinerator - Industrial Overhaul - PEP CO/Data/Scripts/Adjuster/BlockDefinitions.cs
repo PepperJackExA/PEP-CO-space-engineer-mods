7464fa15a369d148e874fc6ed66bb852e5b1a704
PepperJackExA <pepperjack.exa@gmail.com>
2023-04-04 00:45:36 +0300

    initial release

---
+++ 2957201956 - Remove Ice from Incinerator - Industrial Overhaul - PEP CO/Data/Scripts/Adjuster/BlockDefinitions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using static ModAdjuster.DefinitionStructure;
+using static ModAdjuster.DefinitionStructure.BlockDef;
+using static ModAdjuster.DefinitionStructure.BlockDef.BlockAction.BlockMod;
+
+namespace ModAdjuster
+{
+    public class BlockDefinitions
+    {
+        public string AdminComponent = "Component/SomeComponment"; // Component to insert into disabled blocks to prevent building from projection
+        public List<string> DisabledBlocks = new List<string>() // List of blocks to disable
+        {
+
+        };
+
+        public List<BlockDef> Definitions = new List<BlockDef>()
+        {
+
+        };
+
+    }
+}
---
+++ 2957201956 - Remove Ice from Incinerator - Industrial Overhaul - PEP CO/Data/Scripts/Adjuster/BlueprintDefinitions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using static ModAdjuster.DefinitionStructure;
+using static ModAdjuster.DefinitionStructure.BlueprintDef;
+using static ModAdjuster.DefinitionStructure.BlueprintDef.BPAction.BPMod;
+
+namespace ModAdjuster
+{
+    public class BlueprintDefinitions
+    {
+        public List<BlueprintDef> Definitions = new List<BlueprintDef>()
+        {   
+            // List of blueprints to modify. Can be as many or few as desired
+            new BlueprintDef()
+            {
+                BlueprintName = "BlueprintDefinition/IceDisposal",
+                BPActions = new[]
+                {
+                    new BPAction
+                    {
+                        Action = RemovePrerequisite,
+                        Index = 0,
+                    },
+                }
+            },
+
+        };
+    }
+}
