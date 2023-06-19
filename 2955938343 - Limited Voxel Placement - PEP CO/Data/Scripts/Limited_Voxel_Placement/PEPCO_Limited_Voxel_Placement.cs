1043c9dfd323db3d8abe2fc98a9c00ca97f1f1aa
PepperJackExA <pepperjack.exa@gmail.com>
2023-06-19 18:43:36 +0300

    added config file

--- 2955938343 - Limited Voxel Placement - PEP CO/Data/Scripts/Limited_Voxel_Placement/PEPCO_Limited_Voxel_Placement.cs
+++ 2955938343 - Limited Voxel Placement - PEP CO/Data/Scripts/Limited_Voxel_Placement/PEPCO_Limited_Voxel_Placement.cs
@@ -2,37 +2,165 @@
 using VRage.Game;
 using VRage.Game.Components;
 using VRage.Game.ObjectBuilders.Definitions.SessionComponents;
+using System;
+using System.IO;
+using Sandbox.ModAPI;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Definitions;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame.Utilities; // this ingame namespace is safe to use in mods as it has nothing to collide with
+using VRage.Utils;
+using System.Collections.Generic;
+using VRage.Game.Components;
+using System.Linq;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using Sandbox.ModAPI;
 
 namespace PEPCO_Limited_Voxel_Placement
 {
     [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
     public class PEPCO_Limited_Voxel_Placement : MySessionComponentBase
     {
+        PEPCO_Voxel_Settings Settings = new PEPCO_Voxel_Settings();
+
+        public List<string> excludedTypeBlocks;
+        public List<string> excludedSubTypeBlocks;
 
         public override void LoadData()
         {
-            foreach (var def in MyDefinitionManager.Static.GetAllDefinitions())
+
+            Settings.Load();
+
+            excludedTypeBlocks = Settings.ExcludedBlockTypes.Split(' ', ',').ToList();
+
+            excludedSubTypeBlocks = Settings.ExcludedBlockSubTypes.Split(' ', ',').ToList();
+
+            foreach (var def in MyDefinitionManager.Static.GetAllDefinitions()) // Loop through all blocks
             {
                 var blockDef = def as MyCubeBlockDefinition;
-                if (blockDef != null)
+                if (blockDef != null && blockDef.Id.TypeId != typeof(MyObjectBuilder_CubeBlock)) // Filter out all blocks 
                 {
-                    // ignore all CubeBlock and BatteryBlock types
-                    if (blockDef.Id.TypeId == typeof(MyObjectBuilder_CubeBlock) || blockDef.Id.TypeId == typeof(MyObjectBuilder_BatteryBlock) || blockDef.Id.TypeId == typeof(MyObjectBuilder_Conveyor) || blockDef.Id.TypeId == typeof(MyObjectBuilder_ConveyorConnector) || blockDef.Id.TypeId == typeof(MyObjectBuilder_ConveyorSorter) || blockDef.Id.SubtypeId.ToString() == "BasicStaticDrill" || blockDef.Id.SubtypeId.ToString() == "AdvancedStaticDrill" || blockDef.Id.SubtypeId.ToString() == "StaticDrill")
-                            continue;
 
-                    blockDef.VoxelPlacement = new VoxelPlacementOverride()
+
+                    var typeitem = excludedTypeBlocks.Find(x => x == blockDef.Id.TypeId.ToString()); // Find block in the exclusion list
+                    var subtypeitem = excludedSubTypeBlocks.Find(x => x == blockDef.Id.SubtypeId.ToString()); // Find block in the exclusion list
+
+                    if (typeitem == null && subtypeitem == null) // Apply voxel limitation if block wasn't found
                     {
-                        DynamicMode = new VoxelPlacementSettings()
+                        MyLog.Default.WriteLineAndConsole($"### Limited voxel placement of block={blockDef.Id.TypeId.ToString()} {blockDef.Id.SubtypeId.ToString()}");
+
+                        blockDef.VoxelPlacement = new VoxelPlacementOverride()
                         {
-                            PlacementMode = VoxelPlacementMode.OutsideVoxel,
-                        },
-                        StaticMode = new VoxelPlacementSettings()
-                        {
-                            PlacementMode = VoxelPlacementMode.OutsideVoxel,
-                        },
-                    };
+                            DynamicMode = new VoxelPlacementSettings()
+                            {
+                                PlacementMode = VoxelPlacementMode.OutsideVoxel,
+                            },
+                            StaticMode = new VoxelPlacementSettings()
+                            {
+                                PlacementMode = VoxelPlacementMode.OutsideVoxel,
+                            },
+                        };
+                    }
+
+
                 }
             }
         }
     }
+
+    public class PEPCO_Voxel_Settings
+    {
+        const string VariableId = nameof(PEPCO_Limited_Voxel_Placement); // IMPORTANT: must be unique as it gets written in a shared space (sandbox.sbc)
+        const string FileName = "PEPCO_Voxel_Settings.ini"; // the file that gets saved to world storage under your mod's folder
+        const string IniSection = "Voxel_Settings";
+
+        // settings you'd be reading, and their defaults.
+        public string ExcludedBlockTypes = "MyObjectBuilder_BatteryBlock, MyObjectBuilder_Conveyor, MyObjectBuilder_ConveyorConnector";
+        public string ExcludedBlockSubTypes = "BasicStaticDrill, AdvancedStaticDrill, StaticDrill";
+
+        void LoadConfig(MyIni iniParser)
+        {
+            // repeat for each setting field
+            ExcludedBlockTypes = iniParser.Get(IniSection, nameof(ExcludedBlockTypes)).ToString(ExcludedBlockTypes);
+            ExcludedBlockSubTypes = iniParser.Get(IniSection, nameof(ExcludedBlockSubTypes)).ToString(ExcludedBlockSubTypes);
+
+        }
+
+        void SaveConfig(MyIni iniParser)
+        {
+            // repeat for each setting field
+            iniParser.Set(IniSection, nameof(ExcludedBlockTypes), ExcludedBlockTypes);
+            iniParser.SetComment(IniSection, nameof(ExcludedBlockTypes), "This is a list of all excluded block types. They can be placed in voxels"); // optional
+
+            // repeat for each setting field
+            iniParser.Set(IniSection, nameof(ExcludedBlockSubTypes), ExcludedBlockSubTypes);
+            iniParser.SetComment(IniSection, nameof(ExcludedBlockSubTypes), "This is a list of all excluded block subtypes. They can be placed in voxels"); // optional
+
+        }
+
+        // nothing to edit below this point
+
+        public PEPCO_Voxel_Settings()
+        {
+        }
+
+        public void Load()
+        {
+            if (MyAPIGateway.Session.IsServer)
+                LoadOnHost();
+            else
+                LoadOnClient();
+        }
+
+        void LoadOnHost()
+        {
+            MyIni iniParser = new MyIni();
+
+            // load file if exists then save it regardless so that it can be sanitized and updated
+
+            if (MyAPIGateway.Utilities.FileExistsInWorldStorage(FileName, typeof(PEPCO_Voxel_Settings)))
+            {
+                using (TextReader file = MyAPIGateway.Utilities.ReadFileInWorldStorage(FileName, typeof(PEPCO_Voxel_Settings)))
+                {
+                    string text = file.ReadToEnd();
+
+                    MyIniParseResult result;
+                    if (!iniParser.TryParse(text, out result))
+                        throw new Exception($"Config error: {result.ToString()}");
+
+                    LoadConfig(iniParser);
+                }
+            }
+
+            iniParser.Clear(); // remove any existing settings that might no longer exist
+
+            SaveConfig(iniParser);
+
+            string saveText = iniParser.ToString();
+
+            MyAPIGateway.Utilities.SetVariable<string>(VariableId, saveText);
+
+            using (TextWriter file = MyAPIGateway.Utilities.WriteFileInWorldStorage(FileName, typeof(PEPCO_Voxel_Settings)))
+            {
+                file.Write(saveText);
+            }
+        }
+
+        void LoadOnClient()
+        {
+            string text;
+            if (!MyAPIGateway.Utilities.GetVariable<string>(VariableId, out text))
+                throw new Exception("No config found in sandbox.sbc!");
+
+            MyIni iniParser = new MyIni();
+            MyIniParseResult result;
+            if (!iniParser.TryParse(text, out result))
+                throw new Exception($"Config error: {result.ToString()}");
+
+            LoadConfig(iniParser);
+        }
+    }
+
+
 }
