8ceef9d335febfa790d07245b0625bb65a08e5a7
PepperJackExA <93437715+PepperJackExA@users.noreply.github.com>
2023-09-04 20:26:00 -0500

    Industrial Overhaul update

--- 2344068716/Data/Scripts/GeothermalWell.cs
+++ 2344068716/Data/Scripts/GeothermalWell.cs
@@ -29,132 +29,154 @@ using VRage.Utils;
 using VRageMath;
 
 namespace GeothermalWell{
-	
-	[MyEntityComponentDescriptor(typeof(MyObjectBuilder_CargoContainer), false, "GeothermalWellTip")]
-	 
-	public class GeothermalWell : MyGameLogicComponent{
-		
-		public IMyCargoContainer GeothermalWellTip;
-		public IMyInventory Inventory;
-		public bool SetupDone = false;
-		public float BaseGeneration = 1f;
-		public MyDefinitionId HeatDefId;
-		public MyObjectBuilder_InventoryItem HeatItem;
-		public MyPlanet ClosestPlanet;
-		public double TotalMultiplier;
-		
-		
-		
-		public override void Init(MyObjectBuilder_EntityBase objectBuilder){
-			
-			base.Init(objectBuilder);
-			
-			try{
-				
-				NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
-				
-				NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME;
-				
-			}catch(Exception exc){
-				
-				
-				
-			}
-			
-		}
-		
-		public override void UpdateBeforeSimulation100(){
-			
-			if (SetupDone == false){
-				
-				GeothermalWellTip = Entity as IMyCargoContainer;
-				Inventory = (VRage.Game.ModAPI.IMyInventory)GeothermalWellTip.GetInventory();
-				HeatDefId = new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Heat");
-				var content = (MyObjectBuilder_PhysicalObject)MyObjectBuilderSerializer.CreateNewObject(HeatDefId);
-				HeatItem = new MyObjectBuilder_InventoryItem { Amount = 1, Content = content };
-				
-				SetupDone = true;
-				
-			}
-			
-			try{
+  
+  [MyEntityComponentDescriptor(typeof(MyObjectBuilder_CargoContainer), false, "GeothermalWellTip")]
+   
+  public class GeothermalWell : MyGameLogicComponent{
+    
+    public IMyCargoContainer GeothermalWellTip;
+    public IMyInventory Inventory;
+    public bool SetupDone = false;
+    public float BaseGeneration = 1f;
+    public MyDefinitionId HeatDefId;
+    public MyObjectBuilder_InventoryItem HeatItem;
+    public MyPlanet ClosestPlanet;
+    
+    //public double TotalMultiplier;
+    public int EfficiencyReduction = 1;    
+    public double DistanceFromSurface = 0;
+    public float HeatPerDecaTick = 0;
+    
+    public override void Init(MyObjectBuilder_EntityBase objectBuilder){
+      
+      base.Init(objectBuilder);
+      
+      try{
+        
+        NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
+        
+        NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME;
+        
+      }catch(Exception exc){
+
+      }
+      
+    }
+    
+    public override void UpdateBeforeSimulation100(){
+      
+      if (SetupDone == false){
+        
+        GeothermalWellTip = Entity as IMyCargoContainer;
+        Inventory = (VRage.Game.ModAPI.IMyInventory)GeothermalWellTip.GetInventory();
+        HeatDefId = new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Heat");
+        var content = (MyObjectBuilder_PhysicalObject)MyObjectBuilderSerializer.CreateNewObject(HeatDefId);
+        HeatItem = new MyObjectBuilder_InventoryItem { Amount = 1, Content = content };
+        
+        (Entity as IMyTerminalBlock).AppendingCustomInfo += CustomInfo;
+        
+        SetupDone = true;
+        
+      }
+      
+      try{
                 
-				var Grid = GeothermalWellTip.CubeGrid;
-			
-				if ((GeothermalWellTip.IsWorking) && (Grid.IsStatic)){
-			
-					var WellHeadPosition = GeothermalWellTip.GetPosition();
-					double DistanceFromSurface = 0;
-					double DistanceFromCenter = 0;
-					double SurfaceToCenter = 0;
-					float EfficiencyReduction = 0;
-						
-					ClosestPlanet = MyGamePruningStructure.GetClosestPlanet(WellHeadPosition);
-						
-					DistanceFromSurface = Vector3D.Distance((ClosestPlanet.GetClosestSurfacePointGlobal(WellHeadPosition)), WellHeadPosition);
-						
-					DistanceFromCenter = Vector3D.Distance((ClosestPlanet.PositionComp.GetPosition()), WellHeadPosition);
-						
-					SurfaceToCenter = Vector3D.Distance((ClosestPlanet.GetClosestSurfacePointGlobal(WellHeadPosition)), (ClosestPlanet.PositionComp.GetPosition()));
-						
-					if ((DistanceFromCenter < SurfaceToCenter) && (DistanceFromSurface > 150)){
-						
-						var EfficiencyEntityList = new List<MyEntity>();
-						var EfficiencySphere = new BoundingSphereD(WellHeadPosition, 250);
-						MyGamePruningStructure.GetAllEntitiesInSphere(ref EfficiencySphere, EfficiencyEntityList, MyEntityQueryType.Static);
-						
-						//foreach (Entity in EfficiencyEntityList){
-							
-							//if (Entity is IMyCubeGrid){
-								
-								
-						
-					    foreach (var Entity in EfficiencyEntityList){
-							
-							if (Entity is IMyCubeBlock){
-								
-								var Block = Entity as IMyCubeBlock;
-						
-								if (Block.BlockDefinition.SubtypeId.ToString().Contains("GeothermalWellTip")){
-								
-									EfficiencyReduction++;
-									
-								}
-								
-							}
-							
-						}
-						
-						TotalMultiplier = ((BaseGeneration * ((DistanceFromSurface - 150) / 100)) / EfficiencyReduction);
-					
-					}
-					
-				}
-				
-			}catch(Exception exc){
-				
-			}
-			
-        }
-		
-		public override void UpdateBeforeSimulation10(){
-			
-			try{
+        var Grid = GeothermalWellTip.CubeGrid;
+      
+        if ((GeothermalWellTip.IsWorking) && (Grid.IsStatic)){
+      
+          var WellHeadPosition = GeothermalWellTip.GetPosition();
+          //double DistanceFromSurface = 0;
+          double DistanceFromCenter = 0;
+          double SurfaceToCenter = 0;
+          //float EfficiencyReduction = 0;
+            
+          ClosestPlanet = MyGamePruningStructure.GetClosestPlanet(WellHeadPosition);
+            
+          DistanceFromSurface = Vector3D.Distance((ClosestPlanet.GetClosestSurfacePointGlobal(WellHeadPosition)), WellHeadPosition);
+            
+          DistanceFromCenter = Vector3D.Distance((ClosestPlanet.PositionComp.GetPosition()), WellHeadPosition);
+            
+          SurfaceToCenter = Vector3D.Distance((ClosestPlanet.GetClosestSurfacePointGlobal(WellHeadPosition)), (ClosestPlanet.PositionComp.GetPosition()));
+            
+          if ((DistanceFromCenter < SurfaceToCenter) && (DistanceFromSurface > 150)){
+            
+            var EfficiencyEntityList = new List<MyEntity>();
+            var EfficiencySphere = new BoundingSphereD(WellHeadPosition, 250);
+            MyGamePruningStructure.GetAllEntitiesInSphere(ref EfficiencySphere, EfficiencyEntityList, MyEntityQueryType.Static);
+            
+            //foreach (Entity in EfficiencyEntityList){
+              
+              //if (Entity is IMyCubeGrid){
                 
-				var Grid = GeothermalWellTip.CubeGrid;
-			
-				if ((GeothermalWellTip.IsWorking) && (Grid.IsStatic)){
-						
-					Inventory.AddItems((MyFixedPoint)BaseGeneration * (MyFixedPoint)TotalMultiplier, HeatItem.Content);
-					
-				}
-				
-			}catch(Exception exc){
-				
-			}
-			
+                
+              EfficiencyReduction = 0;
+              foreach (var Entity in EfficiencyEntityList){
+              
+              if (Entity is IMyCubeBlock){
+                
+                var Block = Entity as IMyCubeBlock;
+            
+                if (Block.BlockDefinition.SubtypeId.ToString().Contains("GeothermalWellTip")){
+                
+                  EfficiencyReduction++;
+                  
+                }
+                
+              }
+              
+            }
+            
+            //TotalMultiplier = ((BaseGeneration * ((DistanceFromSurface - 150) / 100)) / EfficiencyReduction);
+            HeatPerDecaTick = (float)(BaseGeneration * ((BaseGeneration * ((DistanceFromSurface - 150) / 100)) / EfficiencyReduction) );
+          
+          }
+          
         }
-		
-	}
-	
+        
+      }catch(Exception exc){
+        
+      }
+      
+    }
+    
+    public override void UpdateBeforeSimulation10(){
+      
+      try{
+                
+        var Grid = GeothermalWellTip.CubeGrid;
+      
+        if ((GeothermalWellTip.IsWorking) && (Grid.IsStatic)){
+            
+          //Inventory.AddItems((MyFixedPoint)BaseGeneration * (MyFixedPoint)TotalMultiplier, HeatItem.Content);
+          Inventory.AddItems((MyFixedPoint)HeatPerDecaTick, HeatItem.Content);
+          
+        }
+        
+      }catch(Exception exc){
+        
+      }
+      
+    }
+
+    void CustomInfo(IMyTerminalBlock block, StringBuilder info)
+		{
+      info.Append("Well depth: ").AppendFormat("{0:F1}",DistanceFromSurface).Append("m\n");
+     if(EfficiencyReduction==1){
+
+        info.Append("No nearby wells interfering\n");
+
+     }else if(EfficiencyReduction==2){
+
+        info.Append("1 nearby well interfering\n");
+
+      }else{
+
+        info.Append(EfficiencyReduction-1).Append(" nearby wells interfering\n");
+
+      }
+      info.Append("Max gen: ").AppendFormat("{0:F2}",HeatPerDecaTick/(10f/60)).Append(" heat/s\n");
+    }
+  }
+  
 }
