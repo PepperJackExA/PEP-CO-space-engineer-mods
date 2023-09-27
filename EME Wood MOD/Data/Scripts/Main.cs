bec52a3ff75610566c9daae39d65eb0703928f07
PepperJackExA <93437715+PepperJackExA@users.noreply.github.com>
2023-09-27 14:55:56 -0500

    Wood Log drop working

---
+++ EME Wood MOD/Data/Scripts/Main.cs
@@ -0,0 +1,69 @@
+﻿namespace emeWood
+{
+	using System;
+	using Sandbox.ModAPI;
+	using VRage.Game;
+	using VRage.Game.Components;
+	using VRage.Game.Entity;
+	using Sandbox.Game.Entities;
+	using VRageMath;
+	using VRage.ModAPI;
+	using VRage.ObjectBuilders;
+
+
+
+	[MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+	public class Main : MySessionComponentBase
+	{
+
+		public static Random rand = new Random();
+		
+		public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
+		{
+			if (MyAPIGateway.Session.IsServer )
+			{
+				MyEntities.OnEntityAdd += MyEntities_OnEntityAdd;
+			}
+		}
+
+		private void MyEntities_OnEntityAdd(MyEntity obj)
+		{
+			
+			String usestring = obj.ToString();
+			
+			
+			if (usestring.StartsWith("MyDebrisTr")) //ee
+			{
+
+				double dropammount = 100;
+				IMyEntity tree = obj as IMyEntity;
+				String treetype = tree.Model.AssetName;
+				
+				if (treetype.Contains("Medium"))
+					dropammount *= 0.9;
+				if (treetype.Contains("Dead"))
+					dropammount *= 0.9;
+				if (treetype.Contains("Desert"))
+					dropammount *= 0.9;
+				if (treetype.Contains("Pine"))
+					dropammount *= 1.1;
+				if (treetype.Contains("Snow"))
+					dropammount *= 2.0;
+
+				VRage.MyFixedPoint amount = ((int)(dropammount / 5.0));
+
+				int rnd = rand.Next(1, 5);
+
+				MyObjectBuilder_Component Droplog = MyObjectBuilderSerializer.CreateNewObject<MyObjectBuilder_Component>("WoodLogs");
+				rand.Next(1);
+				//MyFloatingObjects.Spawn(new MyPhysicalInventoryItem(amount, Droplog), pos+(upp+3)+(fww)+((rtt)), fww, upp);
+
+				MyFloatingObjects.Spawn(new MyPhysicalInventoryItem(amount, Droplog), tree.GetPosition(), tree.WorldMatrix.Forward , tree.WorldMatrix.Up); 
+
+			}
+
+		}	
+
+	}
+
+}
