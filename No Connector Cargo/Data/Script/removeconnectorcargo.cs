8884a98f65d96dc790c3958231e83378d517a17f
PepperJackExA <93437715+PepperJackExA@users.noreply.github.com>
2024-06-08 08:17:12 -0500

    not working

---
+++ No Connector Cargo/Data/Script/removeconnectorcargo.cs
@@ -0,0 +1,29 @@
+﻿using ObjectBuilders.Definitions;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Definitions;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using VRage.Game.Components;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using IMyShipConnector = Sandbox.ModAPI.Ingame.IMyShipConnector;
+
+namespace PEPCO.removecargo.No_Connector_Cargo.Data.Script
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_ShipConnector), false)]
+    public class NoConnectorCargo : MyGameLogicComponent
+    {
+        public override void Init(MyObjectBuilder_EntityBase ob)
+        {
+            if (!MyAPIGateway.Session.IsServer)
+                return;
+
+            NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+        public override void UpdateOnceBeforeFrame()
+        {
+            var Cargo = Entity as IMyShipConnector;
+                Cargo.CustomName = "Test123";
+        }
+    }
+}
