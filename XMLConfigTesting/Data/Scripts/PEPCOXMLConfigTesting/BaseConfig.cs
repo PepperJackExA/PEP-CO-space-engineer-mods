e386bcd7cc4afd61e7c2b47cabebf27e6e64e8ae
Unicorn Consulting <66555795+g46492@users.noreply.github.com>
2024-07-14 12:56:04 +0200

    Just some meddling with XML definition inputs

---
+++ XMLConfigTesting/Data/Scripts/PEPCOXMLConfigTesting/BaseConfig.cs
@@ -0,0 +1,42 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using VRage.Game.ModAPI;
+using VRage.Game;
+
+namespace PEPCOXMLConfigTesting
+{
+    [ProtoContract]
+    public class BaseConfig
+    {
+        [XmlAttribute("TheString")]
+        public string TheString = "(null)";
+        [XmlAttribute("TheOtherString")]
+        public string TheOtherString = "(null)";
+
+        [ProtoIgnore, XmlIgnore]
+        public MyDefinitionId DefinitionId;
+
+        [ProtoIgnore, XmlIgnore]
+        public IMyModContext ModContext;
+
+        public virtual void Init(IMyModContext modContext)
+        {
+            ModContext = modContext;
+
+            MyDefinitionId.TryParse(TheString + "/" + SubtypeId, out DefinitionId);
+        }
+
+        public BaseConfig(string typeId, string subtypeId)
+        {
+            TypeId = typeId;
+            SubtypeId = subtypeId;
+        }
+
+        public BaseConfig() { }
+    }
+}
