0bc722dad5ec093caf5fdb23258018de62f67bae
Uzar <66555795+g46492@users.noreply.github.com>
2024-07-18 15:05:17 +0200

    New mod that allows PBs to query Water Mod API stuff

---
+++ Water Mod - PB Interface/Data/Scripts/PEPCO_SubmarineForce/RemoteControl.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Definitions;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Lights;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+using Digi;
+using Sandbox.Game.EntityComponents;
+using static VRageMath.Base6Directions;
+using static VRageMath.Base27Directions;
+using SubmarineStuff;
+using Sandbox.Game.Entities.Cube;
+using Sandbox.Game;
+using VRage;
+using Jakaria.API;
+using VRage.Game.Entity;
+using System.Text;
+
+namespace SubmarineStuff
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_RemoteControl), false)]
+    public class RemoteControlLogic : MyGameLogicComponent
+    {
+
+        IMyRemoteControl block;
+        IMyCubeGrid myCubeGrid;
+
+        public Vector3 BuoyancyForce { get; set; }
+        public Vector3 CenterOfBuoyancy { get; set; }
+        public Vector3 FluidVelocity { get; set; }
+        public float FluidDepth { get; set; }
+        public float PercentUnderwater { get; set; }
+        public float FluidPressure { get; set; }
+        public float BuoyancyRatio { get; set; }
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            if (WaterModAPI.Registered)
+            {
+                RemoteControlTerminalControls.DoOnce(ModContext);
+
+                block = (IMyRemoteControl)Entity;
+                myCubeGrid = block.CubeGrid;
+
+                NeedsUpdate = MyEntityUpdateEnum.EACH_10TH_FRAME;
+            }
+            else
+            {
+                Log.Info("WaterModAPI not registered");
+            }
+
+        }
+
+        public override void UpdateAfterSimulation10()
+        {
+            if (myCubeGrid?.Physics == null)
+                return; // ignore ghost/projected grids
+
+            // Error handling
+            try
+            {
+                BuoyancyForce = WaterModAPI.Entity_BuoyancyForce((MyCubeGrid)myCubeGrid);
+                CenterOfBuoyancy = WaterModAPI.Entity_CenterOfBuoyancy((MyCubeGrid)myCubeGrid);
+                FluidVelocity = WaterModAPI.Entity_FluidVelocity((MyCubeGrid)myCubeGrid);
+                FluidDepth = (float)WaterModAPI.Entity_FluidDepth((MyCubeGrid)myCubeGrid);
+                PercentUnderwater = WaterModAPI.Entity_PercentUnderwater((MyCubeGrid)myCubeGrid);
+                FluidPressure = WaterModAPI.Entity_FluidPressure((MyCubeGrid)myCubeGrid);
+                BuoyancyRatio = BuoyancyForce.Length() / (myCubeGrid.Physics.Mass * myCubeGrid.Physics.Gravity.Length()) * 100;
+            }
+            catch (Exception e)
+            {
+                Log.Info("Error in SubmarineStuff: " + e.Message);
+            }
+
+
+
+        }
+
+    }
+}
---
+++ Water Mod - PB Interface/Data/Scripts/PEPCO_SubmarineForce/RemoteControlTerminalControls.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Jakaria.API;
+using Sandbox.Game.Localization;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces.Terminal;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.Utils;
+using VRageMath;
+
+
+namespace SubmarineStuff
+{
+    public static class RemoteControlTerminalControls
+    {
+        const string IdPrefix = "SubmarineStuffEngine_"; // highly recommended to tag your properties/actions like this to avoid colliding with other mods'
+
+        static bool Done = false;
+
+        // just to clarify, don't store your states/values here, those should be per block and not static.
+
+        public static void DoOnce(IMyModContext context) // called by GyroLogic.cs
+        {
+            if (Done)
+                return;
+            Done = true;
+
+            // these are all the options and they're not all required so use only what you need.
+
+            CreateProperties();
+        }
+
+        static bool CustomVisibleCondition(IMyTerminalBlock b)
+        {
+            // only visible for the blocks having this gamelogic comp
+            return b?.GameLogic?.GetAs<RemoteControlLogic>() != null;
+        }
+        static void CreateProperties()
+        {
+            {
+                var p = MyAPIGateway.TerminalControls.CreateProperty<Vector3, IMyRemoteControl>(IdPrefix + "BuoyancyForce");
+
+                p.Getter = (b) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<RemoteControlLogic>();
+                    if (logic != null) return logic.BuoyancyForce;
+                    else return Vector3.Zero;
+                };
+                p.Setter = (b, v) =>
+                {
+                };
+
+                MyAPIGateway.TerminalControls.AddControl<IMyRemoteControl>(p);
+            }
+
+            {
+                var p = MyAPIGateway.TerminalControls.CreateProperty<Vector3, IMyRemoteControl>(IdPrefix + "CenterOfBuoyancy");
+
+                p.Getter = (b) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<RemoteControlLogic>();
+                    if (logic != null) return logic.CenterOfBuoyancy;
+                    else return Vector3.Zero;
+                };
+                p.Setter = (b, v) =>
+                {
+                };
+
+                MyAPIGateway.TerminalControls.AddControl<IMyRemoteControl>(p);
+            }
+
+            {
+                var p = MyAPIGateway.TerminalControls.CreateProperty<Vector3, IMyRemoteControl>(IdPrefix + "FluidVelocity");
+
+                p.Getter = (b) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<RemoteControlLogic>();
+                    if (logic != null) return logic.FluidVelocity;
+                    else return Vector3.Zero;
+                };
+                p.Setter = (b, v) =>
+                {
+                };
+
+                MyAPIGateway.TerminalControls.AddControl<IMyRemoteControl>(p);
+            }
+
+            {
+                var p = MyAPIGateway.TerminalControls.CreateProperty<float, IMyRemoteControl>(IdPrefix + "FluidDepth");
+
+                p.Getter = (b) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<RemoteControlLogic>();
+                    if (logic != null) return logic.FluidDepth;
+                    else return float.PositiveInfinity;
+                };
+                p.Setter = (b, v) =>
+                {
+                };
+
+                MyAPIGateway.TerminalControls.AddControl<IMyRemoteControl>(p);
+            }
+
+            {
+                var p = MyAPIGateway.TerminalControls.CreateProperty<float, IMyRemoteControl>(IdPrefix + "PercentUnderwater");
+
+                p.Getter = (b) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<RemoteControlLogic>();
+                    if (logic != null) return logic.PercentUnderwater;
+                    else return float.PositiveInfinity;
+                };
+                p.Setter = (b, v) =>
+                {
+                };
+
+                MyAPIGateway.TerminalControls.AddControl<IMyRemoteControl>(p);
+            }
+
+            {
+                var p = MyAPIGateway.TerminalControls.CreateProperty<float, IMyRemoteControl>(IdPrefix + "FluidPressure");
+
+                p.Getter = (b) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<RemoteControlLogic>();
+                    if (logic != null) return logic.FluidPressure;
+                    else return float.PositiveInfinity;
+                };
+                p.Setter = (b, v) =>
+                {
+                };
+
+                MyAPIGateway.TerminalControls.AddControl<IMyRemoteControl>(p);
+            }
+
+            {
+                var p = MyAPIGateway.TerminalControls.CreateProperty<float, IMyRemoteControl>(IdPrefix + "BuoyancyRatio");
+
+                p.Getter = (b) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<RemoteControlLogic>();
+                    if (logic != null) return logic.BuoyancyRatio;
+                    else return float.PositiveInfinity;
+                };
+                p.Setter = (b, v) =>
+                {
+                };
+
+                MyAPIGateway.TerminalControls.AddControl<IMyRemoteControl>(p);
+            }
+
+
+
+        }
+
+    }
+}
