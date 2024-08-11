9962194296995521c53a0cf7d54b553691583c73
PepperJackExA <pepperjack.exa@gmail.com>
2024-08-10 21:44:49 -0400

    Fixed power and Added Water API support

---
+++ 3281335144 - Custom Entity Spawner - DEV/Data/Scripts/CES/API/APIUsageExample.cs
@@ -0,0 +1,41 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jakaria.API;
+using Sandbox.Game.Entities;
+using Sandbox.Game.GameSystems;
+using Sandbox.ModAPI;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI.Ingame;
+using VRageMath;
+
+namespace Jakaria
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+    class APIUsageExample : MySessionComponentBase
+    {
+        public override void BeforeStart()
+        {
+            if (WaterModAPI.Registered)
+            {
+                MyAPIGateway.Utilities.ShowMessage("Mod", "Water API is registered");
+            }
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            MyEntity entity = (MyEntity)(MyAPIGateway.Session?.Player?.Controller?.ControlledEntity ?? null);
+
+            if(entity != null)
+            {
+                MyAPIGateway.Utilities.ShowNotification($"Depth: {WaterModAPI.Entity_FluidDepth(entity)}", 16);
+                MyAPIGateway.Utilities.ShowNotification($"%Underwater: {WaterModAPI.Entity_PercentUnderwater(entity) * 100}%", 16);
+                MyAPIGateway.Utilities.ShowNotification($"Pressure: {WaterModAPI.Entity_FluidPressure(entity)}kPa", 16);
+            }
+        }
+    }
+}
+*/
